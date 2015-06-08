9e855a6473710abd0f906d37526e83a727d9e3a9
Navpreet Singh <navpreet.singh.k@gmail.com>
2015-06-08 11:59:46 +0530

    modified view

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,8 +4,6 @@ elsif Rails.env == "production"
 	AuthDetails = YAML.load_file(File.join(Rails.root, 'config', 'auth_production.yml'))
 end
 
-
-
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :google_oauth2, AuthDetails["GOOGLE_CLIENT_ID"], 
 	AuthDetails["GOOGLE_CLIENT_SECRET"], 
